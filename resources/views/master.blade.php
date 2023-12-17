0f8b7e72a08fe972932214858fb4930d2bce481a
indra.permadi21 <indrap.git21@gmail.com>
2023-12-17 23:34:29 +0700

    update register

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aplikasi Nilai Santri</title>
+    <title>Admin Login</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
